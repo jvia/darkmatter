9bfce90b505a38c61806132132af2fd5c21146c3
Jeremiah Via <jeremiah.via@gmail.com>
2011-01-28 13:33:43 +0000

    Adding package level javadoc for the sandbox.

---
+++ src/main/java/sanbox/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package will hold prototype code.
+ *
+ * Code in here should not be referenced by other packages.
+ * It will be a place to clear thoughts and work on ideas
+ * without impacting the mainline code.
+ */
+package sanbox;
