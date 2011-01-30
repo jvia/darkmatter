572c87011b7278b3a0b973675b5887d313807e41
Jeremiah Via <jeremiah.via@gmail.com>
2011-01-30 22:40:43 +0000

    This is the result after spending Sunday hacking on some prototype code. It has some of the desired game functionality but poorly implemented. It looks like the basic structure of the game may be straightforward but it will take a lot of fine-tuning to make it work well.
    The class I made plays around with some other stuff, too. For example,
    it goes fullscreen which will be handy to know. Although doing that
    introduces some other bugs which will need to be worked out.
    
    There are a ton of bugs in this. The structure is terrible and
    ugly. This is just to toy around with some of the basic
    mechanics. Let's not actually create an architecture like this!
    
    * Bugs
      
      - On one occasion circle coordinates became NaN and circle disappeared
      - Collision detection is painfully bad
      - Change in momentum calculations are *very* wrong
      - Circles are generated too far from player's circle
      - Many crashes due to concurrent modifications of the list structure

--- src/main/java/sanbox/CalculatingMovement.java
+++ src/main/java/sanbox/CalculatingMovement.java
@@ -2,6 +2,8 @@ package sanbox;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.geom.Ellipse2D;
@@ -10,6 +12,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Bugs:
+ * - On one occasion circle coordinates became NaN and circle disappeared
+ * - Collision detection is painfully bad
+ * - Change in momentum calculations are *very* wrong
+ * - Circles are generated too far from player's circle
+ * - Many crashes due to concurrent modifications of the list structure
+ *
  * @author via
  */
 public class CalculatingMovement {
@@ -18,15 +27,54 @@ public class CalculatingMovement {
     public static int WIDTH = 400;
     public static int HEIGHT = 400;
 
-    public CalculatingMovement() {
+    public CalculatingMovement() {         
+ 		
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         frame = new JFrame();
-        panel = new CirclePanel();
-        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setBackground(new Color(26, 23, 19));
+
+        // Add key listener
+        frame.addKeyListener(new KeyListener() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    frame.dispose();
+                    System.exit(0);
+                }
+            }
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+                // not used
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                //not used
+            }
+        });
+
+        panel = new CirclePanel();
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        panel.setPreferredSize(dim);
+        frame.setUndecorated(true);
+        //panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        frame.add(panel);
         frame.addMouseListener(panel);
-        frame.pack();
-        frame.setVisible(true);
+        
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+ 		frame.setUndecorated(true);
+ 		frame.setResizable(false);
+ 		frame.validate();
+
+ 		GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().setFullScreenWindow(frame);
+//
+//        frame.pack();
+//        frame.setVisible(true);
+        
+        
     }
 
     public boolean isOver() {
@@ -49,23 +97,38 @@ public class CalculatingMovement {
 
     private static void updateLogic() {
         // See if there is a collision
+        double percent = 0.7;
         Circle player = panel.circles.get(0);
+        ArrayList<Circle> nextCircles = new ArrayList<Circle>(panel.circles.size());
+        nextCircles.add(player);
+
         for (Circle npc : panel.circles.subList(1, panel.circles.size())) {
             double x = Math.abs(player.getCenterX() - npc.getCenterX());
             double y = Math.abs(player.getCenterY() - npc.getCenterY());
             double dist = Math.hypot(x, y);
 
-            // check for collision
+            // check for collisions
             if (dist <= player.getHeight() + npc.getHeight()) {
-                if (player.getArea() >= npc.getArea())
+                if (player.getArea() >= npc.getArea()) {
                     System.out.println("Absorb! @ " + System.nanoTime());
-                else
+                    player.setArea(player.getArea() + (npc.getArea() * percent));
+                    player.setDX((player.getDX() + npc.getDX()) / 2);
+                    player.setDY((player.getDY() + npc.getDY()) / 2);
+                    npc.setArea(npc.getArea() - (npc.getArea() * percent));
+                } else {
                     System.out.println("Absorbed! @ " + System.nanoTime());
 
+                }
+
             }
+
+            if (npc.getArea() > 1.0)
+                nextCircles.add(npc);
+
         }
 
 
+        panel.circles = nextCircles;
         for (Circle c : panel.circles) {
             c.update();
         }
@@ -74,14 +137,16 @@ public class CalculatingMovement {
 
 
 /**
- * Rerpresents a circle object on the screen.
+ * Represents a circle object on the screen.
  */
 class Circle extends Ellipse2D.Double {
     private double dy;
     private double dx;
+    private double radius;
 
     public Circle(final double x, final double y, final double r) {
         super(x, y, r, r);
+        radius = r;
         dy = 0;
         dx = 0;
     }
@@ -92,13 +157,10 @@ class Circle extends Ellipse2D.Double {
         this.dx = dx;
     }
 
-    public Circle(final double dy, final double dx) {
-        this(0.0, 0.0, 0.0, dy, dx);
-    }
-
     public void setRadius(final double r) {
         this.width = r;
         this.height = r;
+        radius = r;
     }
 
     public void setDY(final double dy) {
@@ -122,10 +184,10 @@ class Circle extends Ellipse2D.Double {
     }
 
     public void update() {
-        if (this.getMaxX() >= CalculatingMovement.WIDTH || x <= 0) {
+        if (this.getMaxX() >= CalculatingMovement.panel.getWidth() || x <= 0) {
             dx = -dx;
         }
-        if (this.getMaxY() >= CalculatingMovement.HEIGHT || y <= 0) {
+        if (this.getMaxY() >= CalculatingMovement.panel.getHeight() || y <= 0) {
             dy = -dy;
         }
 
@@ -158,6 +220,15 @@ class Circle extends Ellipse2D.Double {
         deltaY /= normalizer;
         return deltaY;
     }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setArea(double area) {
+        double radius = Math.sqrt(area / Math.PI);
+        setRadius(radius);
+    }
 }
 
 /**
@@ -168,11 +239,10 @@ class CirclePanel extends JPanel implements MouseListener {
     private Circle circle;
     ArrayList<Circle> circles;
     private Point2D click = new Point2D.Double();
-    private String msg = "HI";
 
     public CirclePanel() {
-        setBackground(new Color(224, 228, 204));
-        circle = new Circle(185, 185, 30);
+        setBackground(new Color(20, 47, 48));
+        circle = new Circle(400, 600, 100);
         circles = new ArrayList<Circle>();
         circles.add(circle);
         this.setSize(CalculatingMovement.WIDTH, CalculatingMovement.HEIGHT);
@@ -182,11 +252,6 @@ class CirclePanel extends JPanel implements MouseListener {
         // Draw player's circle
         g2.setColor(new Color(243, 134, 48));
         g2.fill(player);
-        g2.setColor(Color.red);
-
-        // Draw boundng box
-        g2.setColor(Color.red);
-        g2.draw(player.getBounds2D());
     }
 
     private void drawNPC(Graphics2D g2, List<Circle> circles) {
@@ -194,8 +259,6 @@ class CirclePanel extends JPanel implements MouseListener {
         for (Circle c : circles) {
             g2.setColor(new Color(167, 219, 216));
             g2.fill(c);
-            g2.setColor(Color.red);
-            g2.draw(c.getBounds2D());
         }
 
     }
@@ -211,25 +274,40 @@ class CirclePanel extends JPanel implements MouseListener {
 
 
         g2.setColor(Color.red);
-        g2.drawString(msg, 10, 290);
-
-
+        g2.drawString("Hit [ESC] to Quit", 15, 15);
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        msg = String.format("Circle: (%.2f, %.2f)    Click: (%d, %d) ",
-                circle.x, circle.y, e.getX(), e.getY());
         click.setLocation(e.getX(), e.getY());
-
-
         // Create a new circle from the player's circle
-        double r = circle.radiusOfAreaPercentage(0.10);
-        double dx = circle.changeInX(e.getX(), e.getY());
-        double dy = circle.changeInY(e.getX(), e.getY());
-        System.out.printf("DX: %5.2f  DY: %5.2f   DY/DX: %5.2f\n", dy, dx, dy / dx);
-        Circle matter = new Circle(circle.getX(), circle.getY(), r, dy, dx);
+        // and shrink player's circle
+        Circle matter = createCircle(e.getX(), e.getY(), circle);
+        circle.setRadius(circle.radiusOfAreaPercentage(0.90));
+        circle.setDY(circle.getDY() - matter.getDY());
+        circle.setDX(circle.getDX() - matter.getDX());
         circles.add(matter);
+    }
+
+    private Circle createCircle(int clickX, int clickY, Circle circle) {
+        double r = circle.radiusOfAreaPercentage(0.10);
+        double dx = circle.changeInX(clickX, clickY);
+        double dy = circle.changeInY(clickX, clickY);
+        // Calculate the X & Y coordinates of the circle
+        double centerX = circle.getCenterX();
+        double centerY = circle.getCenterY();
+        double x1 = circle.getCenterX();
+        double y1 = circle.getCenterY();
+
+        if (clickX < centerX)
+            x1 -= circle.getRadius() + r;
+        if (clickX > centerX)
+            x1 += circle.getRadius();
+        if (clickY < centerY)
+            y1 -= circle.getRadius() + r;
+        if (clickY > centerY)
+            y1 += circle.getRadius();
+        return new Circle(x1, y1, r, dy, dx);
     }
 
     @Override
