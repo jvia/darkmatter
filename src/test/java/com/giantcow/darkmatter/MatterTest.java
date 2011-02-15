d441edc096b75a4e5a0327a04b24ed0730a1a044
Jeremiah Via <jeremiah.via@gmail.com>
2011-02-15 10:48:00 +0000

    Removing JOrbis dependency until it is fixed.

--- src/test/java/com/giantcow/darkmatter/MatterTest.java
+++ src/test/java/com/giantcow/darkmatter/MatterTest.java
@@ -41,6 +41,17 @@ public class MatterTest extends TestCase {
         
     }
 
+    /*
+    public void testIntersects2() {
+        System.out.println("One point intersection");
+        Matter other = new Matter(2.0, 0.0, 1.0, 0.0, 0.0);
+        Matter instance = new Matter(0.0, 0.0, 1.0, 0.0, 0.0);
+        boolean expResult = true;
+        boolean result = instance.intersects(other);
+        assertEquals(expResult, result);
+    }
+    */
+
     /**
      * Test of update method, of class Matter.
      */
