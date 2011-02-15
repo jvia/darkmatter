8930c3ec253ee3146592eb31a14000e9d95c6ed6
Joss Greenaway <jtg897@cs.bham.ac.uk>
2011-02-15 23:46:27 +0000

    Fixed MusicPlayer so that it will take a name of a file, retrieve the URL path from that and then convert that to an AbsolutePath which can be used to make a File which can be played. Works fine on Windows, need to test it on Linux and see if there are any glaring issues I've missed out on.

--- src/main/java/com/giantcow/darkmatter/MusicPlayer.java
+++ src/main/java/com/giantcow/darkmatter/MusicPlayer.java
@@ -1,7 +1,10 @@
 package com.giantcow.darkmatter;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -20,33 +23,73 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 public class MusicPlayer extends Thread {
 
     private String fileName;
-    private Position curPosition;
+    private Position chPosition;
     private final int EXTERNAL_BUFFER_SIZE = 524288;
 
     enum Position {
+
         LEFT, RIGHT, NORMAL
     };
 
+    /**
+     * Constructor for a MusicPlayer object where the only parameter is the File Name,
+     * automatically sets the Position to normal
+     * @param wavFile String representing the name of the File to be played
+     */
     public MusicPlayer(String wavFile) {
         fileName = wavFile;
-        curPosition = Position.NORMAL;
+        chPosition = Position.NORMAL;
     }
 
+    /**
+     * Constructor for a MusicPlayer object with the parameters of the File Name
+     * and the Position
+     * @param wavFile String representing the name of the File to be played
+     * @param p Position of the channel the audio will be played through
+     */
     public MusicPlayer(String wavFile, Position p) {
         fileName = wavFile;
-        curPosition = p;
+        chPosition = p;
     }
 
     @Override
     public void run() {
 
-        //File soundFile = new File((getClass().getClassLoader().getResource(fileName)).getFile());
-        File soundFile = new File("C:\\Users\\Joss\\Documents\\Work Directory\\Netbeans Projects\\DarkMatter\\trunk\\target\\classes\\SpaceFighterLoop.wav");
-        if (!soundFile.exists()) {
-            System.err.println("Wave file not found : " + fileName);
-            System.err.println("Target file : " + getClass().getClassLoader().getResource(fileName).getFile());
-            return;
+        //Retrieves a URL of the File to be played, and assigns it's URL path to a String
+        String filePath = getClass().getClassLoader().getResource(fileName).getPath();
+
+        /* Removes the escape character at the start of the URL path, converting
+         * the rest to a character array and iterating through to remove any
+         * occurances of '%20', the URL character for spaces
+         */
+        StringBuilder buf = new StringBuilder(filePath.length() - 1);
+        buf.append(filePath.substring(0, 0)).append(filePath.substring(1));
+        filePath = buf.toString();
+        filePath = filePath.replace('%', ' ');
+        char[] ca = filePath.toCharArray();
+        char[] ca2 = new char[filePath.length()];
+
+        int counter = 0;
+        for (int i = 0; i < ca.length; i++) {
+            
+            if (ca[i] == '2' && ca[i + 1] == '0') {
+                i = i + 1;
+                counter++;
+            } else {
+                ca2[i - (counter*2)] = ca[i];
+                
+            }
+
         }
+        filePath = new String(ca2);
+
+        // Creates a new File from the Absolute File Path that's been generated and checks it exists
+            File soundFile = new File(filePath);
+            if (!soundFile.exists()) {
+                System.err.println("Wave file not found : " + fileName);
+                return;
+            }
+        
 
         AudioInputStream audioIn = null;
         try {
@@ -76,9 +119,9 @@ public class MusicPlayer extends Thread {
 
         if (auline.isControlSupported(FloatControl.Type.PAN)) {
             FloatControl pan = (FloatControl) auline.getControl(FloatControl.Type.PAN);
-            if (curPosition == Position.RIGHT) {
+            if (chPosition == Position.RIGHT) {
                 pan.setValue(1.0f);
-            } else if (curPosition == Position.LEFT) {
+            } else if (chPosition == Position.LEFT) {
                 pan.setValue(-1.0f);
             }
         }
@@ -104,8 +147,13 @@ public class MusicPlayer extends Thread {
         }
 
 
+
     }
 
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         new MusicPlayer("SpaceFighterLoop.wav").start();
     }
