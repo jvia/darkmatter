37b4a1d58a7a3fd96010a77e6b8f802d53edf115
Yukun Wang <yxw999@cs.bham.ac.uk>
2011-02-17 14:50:42 +0000

    add some new test method

--- src/test/java/com/giantcow/darkmatter/VelocityVectorTest.java
+++ src/test/java/com/giantcow/darkmatter/VelocityVectorTest.java
@@ -7,6 +7,7 @@ import junit.framework.TestCase;
  *
  * @author Charlie Horrel <cxh900@cs.bham.ac.uk>
  * @author Jeremiah Via <jxv911@cs.bham.ac.uk>
+ * @author Yukun Wang <yxw999@cs.bham.ac.uk>
  */
 public class VelocityVectorTest extends TestCase {
     
@@ -19,7 +20,7 @@ public class VelocityVectorTest extends TestCase {
      * Test of resultant method, of class VelocityVector.
      */
     public void testResultant() {
-        System.out.println("resultant");
+        System.out.println("resultant with +dx and +dy");
         VelocityVector instance = new VelocityVector(3.0, 4.0);
         double expResult = 5.0;
         double result = instance.resultant();
@@ -27,29 +28,107 @@ public class VelocityVectorTest extends TestCase {
       
     }
 
+    public void testResultant2(){
+        System.out.println("resultane with -dx and +dy");
+        VelocityVector instance = new VelocityVector(-3.0, 4.0);
+        double expResult = 5.0;
+        double result = instance.resultant();
+        assertEquals(expResult, result, 0.0);
+    }
+
+    public void testResultant3(){
+        System.out.println("resultane with +dx and -dy");
+        VelocityVector instance = new VelocityVector(3.0, -4.0);
+        double expResult = 5.0;
+        double result = instance.resultant();
+        assertEquals(expResult, result, 0.0);
+    }
+
+    public void testResultant4(){
+        System.out.println("resultane with -dx and -dy");
+        VelocityVector instance = new VelocityVector(-3.0, -4.0);
+        double expResult = 5.0;
+        double result = instance.resultant();
+        assertEquals(expResult, result, 0.0);
+    }
+
     /**
      * Test of angle method, of class VelocityVector.
      */
     public void testAngle() {
-        System.out.println("angle");
-        VelocityVector instance = new VelocityVector(3.0, 4.0);
-        double expResult = 53.0;
+        System.out.println("angle with +dx and +dy");
+        VelocityVector instance = new VelocityVector(2.0, 2.0);
+        double expResult = 45.0;
         double result = instance.angle();
-        assertEquals(expResult, result, 0.25);
+        assertEquals(expResult, result, 0);
        
     }
+
+    public void testAngle2(){
+        System.out.println("angle with -dx and +dy");
+        VelocityVector instance = new VelocityVector(-2.0, 2.0);
+        double expResult = 135.0;
+        double result = instance.angle();
+        assertEquals(expResult, result, 0);
+
+    }
+
+    public void testAngle3(){
+        System.out.println("angle with +dx and -dy");
+        VelocityVector instance = new VelocityVector(+2.0, -2.0);
+        double expResult = -45.0;
+        double result = instance.angle();
+        assertEquals(expResult, result, 0);
+
+    }
+    
+    public void testAngle4(){
+        System.out.println("angle with -dx and -dy");
+        VelocityVector instance = new VelocityVector(-2.0, -2.0);
+        double expResult = -135.0;
+        double result = instance.angle();
+        assertEquals(expResult, result, 0);
+
+    }
+
+
 
     /**
      * Test of momentum method, of class VelocityVector.
      */
     public void testMomentum() {
-        System.out.println("momentum");
+        System.out.println("momentum with +dx and +dy");
         double mass = 10.0;
         VelocityVector instance = new VelocityVector(3.0, 4.0);
         double expResult = 50.0;
         double result = instance.momentum(mass);
-        assertEquals(expResult, result, 0.0);
-       
+        assertEquals(expResult, result, 0.0);       
     }
 
+    public void testMomentum2() {
+        System.out.println("momentum with -dx and +dy");
+        double mass = 10.0;
+        VelocityVector instance = new VelocityVector(-3.0, 4.0);
+        double expResult = 50.0;
+        double result = instance.momentum(mass);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    public void testMomentum3() {
+        System.out.println("momentum with +dx and -dy");
+        double mass = 10.0;
+        VelocityVector instance = new VelocityVector(3.0, -4.0);
+        double expResult = 50.0;
+        double result = instance.momentum(mass);
+        assertEquals(expResult, result, 0.0);       
+    }
+
+    public void testMomentum4() {
+        System.out.println("momentum with -dx and -dy");
+        double mass = 10.0;
+        VelocityVector instance = new VelocityVector(-3.0, -4.0);
+        double expResult = 50.0;
+        double result = instance.momentum(mass);
+        assertEquals(expResult, result, 0.0);
+    }
 }
