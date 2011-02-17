00e1c80435101e41848377b7fca61e56760ae15d
Yukun Wang <yxw999@cs.bham.ac.uk>
2011-02-17 03:54:09 +0000

    use the new data structures and the way the objects painted.
    change the absorbed area when two matters intersect. The new area is more accuracy.
    
    add the background of centerView file, to check the situation when matter is close to the screen side.
    
    Background.png is temporary picture just for centerView test.
    
    next step, I should to solve the situation when matter is close to screen side, the screen will paint outside of map.

--- src/main/java/com/giantcow/darkmatter/DarkMatter_CenterView.java
+++ src/main/java/com/giantcow/darkmatter/DarkMatter_CenterView.java
@@ -7,44 +7,49 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.geom.Rectangle2D;
-import java.util.ArrayList;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import javax.imageio.ImageIO;
 
 /**
- * This is the class that runs the whole show.
+ * This class use DarkMatter
  *
  * @author Jeremiah M. Via <jxv911@cs.bham.ac.uk>
  * @author Yukun Wang <yxw999@cs.bham.ac.uk>
  */
 public class DarkMatter_CenterView extends JComponent implements KeyListener, MouseListener {
 
-    private static final long DELAY = 10;
-
     public static void main(String[] args) {
         DarkMatter_CenterView darkMatter = new DarkMatter_CenterView();
         darkMatter.run();
     }
     // GUI Variables
-    private static int DEFAULT_WIDTH = 600;
-    private static int DEFAULT_HEIGHT = 400;
-    private Color background = new Color(14, 36, 48);
+    private static final long DELAY = 10;
+    private static int DEFAULT_WIDTH = 800;
+    private static int DEFAULT_HEIGHT = 600;
+    private BufferedImage background = null;
     private Color player = new Color(252, 58, 81);
     private Color npc = new Color(245, 179, 73);
     private boolean isFullScreen;
-    private double zoom = 1;
-    private int x = 0;
-    private int y = 0;
-    private int width = DEFAULT_WIDTH;
-    private int height = DEFAULT_HEIGHT;
+    private double zoom = 1; //zoomlevel
+    // the distance pen need to move
+    private double x = 0;
+    private double y = 0;
     // Game Variables
-    private boolean gameOver;
-    private ArrayList<Matter> matterList;
+    private Set<Matter> matterList;
     private Matter localPlayer;
     private Matter remotePlayer;
     private double goalArea;
     // Game constants
-    private static double MAX_SPEED = 0.5;
     private static int INITIAL_BLOBS = 20;
 
+    /**
+     * Create the game object.
+     */
     public DarkMatter_CenterView() {
         // Setup our component
         setDoubleBuffered(true);
@@ -57,7 +62,6 @@ public class DarkMatter_CenterView extends JComponent implements KeyListener, Mo
 
         // Add it to the frame
         JFrame frame = new JFrame("DarkMatter");
-        frame.setBackground(background);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(this);
         frame.pack();
@@ -72,8 +76,7 @@ public class DarkMatter_CenterView extends JComponent implements KeyListener, Mo
      * TODO Use level loader to build
      */
     private void init() {
-        gameOver = false;
-        matterList = new ArrayList<Matter>();
+        matterList = new HashSet<Matter>();
         localPlayer = new Matter(20, 20, 40, 0, 0);
         remotePlayer = new Matter(300, 200, 40, 0, 0);
         matterList.add(localPlayer);
@@ -84,55 +87,78 @@ public class DarkMatter_CenterView extends JComponent implements KeyListener, Mo
             // Generate random blob
             Matter m = new Matter(Math.random() * (getWidth() - 50),
                     Math.random() * (getHeight() - 50),
-                    Math.random() * 50,
+                    Math.random() * 25,
                     -0.1 + Math.random() * 0.2,
                     -0.1 + Math.random() * 0.2);
 
             // Check to see if it is on top of any other blobs
             // and if not add it to the queue
-            boolean collides = false;
-            for (Matter other : matterList) {
-                if (m.intersects(other)) {
-                    collides = true;
-                    break;
-                }
-            }
-            if (!collides) {
+            if (!m.intersects(matterList)) {
                 matterList.add(m);
                 area += m.getArea();
+
             }
         }
 
-        goalArea = 0.85 * area;
+        //read the background picture
+        try {
+            background = ImageIO.read(new File("src/main/resources/Background.png"));
+        } catch (IOException e) {
+            System.err.print("Failed to open image file");
+            System.exit(1);
+        }
+        ;
 
+        goalArea = 0.85 * area;
 
     }
 
-
     /**
-     * set the zoom level depends on localplayer's size
+     * calculate the zoomlevel and the distancae pen should move
+     * the initial zoomlevel is 5 times, means the screen width is five times of
+     * local player matter's diameter if it is possible
      */
     private void zoom() {
         if (DEFAULT_HEIGHT > 10 * localPlayer.getRadius()) {
             zoom = DEFAULT_HEIGHT / localPlayer.getRadius() / 10;
         }
 
-        x = (int) (localPlayer.getCenterX()-5*localPlayer.getRadius()*DEFAULT_WIDTH/DEFAULT_HEIGHT);
-        y = (int) (localPlayer.getCenterY()-5*localPlayer.getRadius());
+        /**
+         if (localPlayer.getCenterX() > 5 * localPlayer.getRadius()
+                && DEFAULT_WIDTH - localPlayer.getCenterX() < 5 * localPlayer.getRadius()) {
+            x = localPlayer.getCenterX() - 5 * localPlayer.getRadius() * DEFAULT_WIDTH / DEFAULT_HEIGHT;
+        }
+        if (localPlayer.getCenterY() > 5 * localPlayer.getRadius()
+                && DEFAULT_HEIGHT - localPlayer.getCenterY() < 5 * localPlayer.getRadius()) {
+            y = localPlayer.getCenterY() - 5 * localPlayer.getRadius();
+        }
+         */
+        x = localPlayer.getCenterX() - 5 * localPlayer.getRadius() * DEFAULT_WIDTH / DEFAULT_HEIGHT;
+        y = localPlayer.getCenterY() - 5 * localPlayer.getRadius();
+
     }
 
+    /**
+     * Paints the current state of the game to the screen.
+     *
+     * @param g the graphics context object
+     */
+    @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setColor(background);
 
-        g2.scale(zoom, zoom);
-        g2.translate(-x, -y);
-        g2.fill(this.getBounds());
+        g2.scale(zoom, zoom); //enlarge whole map
+        g2.translate(-x, -y); //move the pen to the right place
 
-        //g2.translate(x, y);
-        for (Matter m : matterList) {
+        g2.drawImage(background, 0, 0, this); //draw the background
+        // Paint in a sorted order so smaller objects are painted below bigger ones
+        Matter[] ms = new Matter[matterList.size()];
+        matterList.toArray(ms);
+        Arrays.sort(ms);
+
+        for (Matter m : ms) {
             if (m.equals(localPlayer) || m.equals(remotePlayer)) {
                 g2.setColor(player);
             } else {
@@ -154,16 +180,20 @@ public class DarkMatter_CenterView extends JComponent implements KeyListener, Mo
         g.dispose();
     }
 
+    /**
+     * This method runs the game. It simple updates every object,
+     * repaints the screen and then sleep for the specified amount
+     * of time.
+     */
     public void run() {
         long beforeTime, timeDiff, sleep;
         beforeTime = System.currentTimeMillis();
 
         while (true) {
 
+            zoom();
             update();
             repaint();
-
-            zoom();
 
             timeDiff = System.currentTimeMillis() - beforeTime;
             sleep = DELAY - timeDiff;
@@ -181,27 +211,39 @@ public class DarkMatter_CenterView extends JComponent implements KeyListener, Mo
         }
     }
 
+    /**
+     * Updates every matter object in the game. This method will get long
+     * so it will be best to try and split it up into many smaller methods.
+     */
     private void update() {
         // Detect collisions
-        for (int i = 0; i < matterList.size(); i++) {
-            for (int j = 0; j < matterList.size(); j++) {
-                if (i == j) continue;
+        for (Matter m : matterList) {
+            for (Matter n : matterList) {
+                if (m.equals(n)) {
+                    continue;
+                }
 
-                Matter m = matterList.get(i);
-                Matter n = matterList.get(j);
                 if (m.intersects(n) && m.isBigger(n)) {
-                    double area = 0.03 * n.getArea();
-                    m.setArea(m.getArea() + area);
-                    n.setArea(n.getArea() - area);
+                    if (m.completelyConsumes(n)) {
+                        m.setArea(m.getArea() + n.getArea());
+                        n.setArea(0.0);
+                    } else {
+                        double d = n.getRadius() + m.getRadius()
+                                - Math.hypot(m.getCenterX() - n.getCenterX(), m.getCenterY() - n.getCenterY());
+                        double area = 0.03 * d * n.getArea();
+                        //double area = 0.03 * n.getArea();
+                        m.setArea(m.getArea() + area);
+                        n.setArea(n.getArea() - area);
+                    }
 
                     // Now changeMove the blob if it is outside the the screen
                     double xOffset = m.getBounds2D().getMaxX() - getWidth();
                     if (xOffset > 0) {
                         Rectangle2D boundingBox = m.getBounds2D();
                         boundingBox.setFrame(boundingBox.getX() - xOffset,
-                                             boundingBox.getY(),
-                                             boundingBox.getWidth(),
-                                             boundingBox.getHeight());
+                                boundingBox.getY(),
+                                boundingBox.getWidth(),
+                                boundingBox.getHeight());
                         m.setFrame(boundingBox);
                     }
 
@@ -210,34 +252,35 @@ public class DarkMatter_CenterView extends JComponent implements KeyListener, Mo
                     if (yOffset > 0) {
                         Rectangle2D boundingBox = m.getBounds2D();
                         boundingBox.setFrame(boundingBox.getX(),
-                                             boundingBox.getY() - yOffset,
-                                             boundingBox.getWidth(),
-                                             boundingBox.getHeight());
+                                boundingBox.getY() - yOffset,
+                                boundingBox.getWidth(),
+                                boundingBox.getHeight());
                         m.setFrame(boundingBox);
                     }
                 }
             }
         }
-
-
         // Change dy/dx if necessary
         for (Matter m : matterList) {
-            if (m.getX() <= 0 || m.getMaxX() >= getWidth())
+            if (m.getX() <= 0 || m.getMaxX() >= getWidth()) {
                 m.setDx(-m.getDx());
-            if (m.getY() <= 0 || m.getMaxY() >= getHeight())
+            }
+            if (m.getY() <= 0 || m.getMaxY() >= getHeight()) {
                 m.setDy(-m.getDy());
+            }
             m.update();
         }
     }
 
     /**
-     * Invoked when a key has been pressed. See the class description for {@link java.awt.event.KeyEvent} for a
-     * definition of a key pressed event.
+     * Invoked when a key has been pressed. See the class description for
+     * {@link java.awt.event.KeyEvent} for a definition of a key pressed
+     * event.
      *
      * @param e the key event
      */
     @Override
-    public void keyPressed(KeyEvent e) {
+    public void keyPressed(final KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_F11) {
             isFullScreen = (isFullScreen) ? false : true;
             System.out.println("Switch modes");
@@ -247,12 +290,13 @@ public class DarkMatter_CenterView extends JComponent implements KeyListener, Mo
     /** Invoked when a mouse button has been pressed on a component. */
     @Override
     public void mousePressed(MouseEvent e) {
-        Matter m = localPlayer.changeMove(e.getX()/zoom+x , e.getY()/zoom+y);
+        Matter m = localPlayer.changeMove(e.getX() / zoom + x, e.getY() / zoom + y);
         if (m != null) {
             matterList.add(m);
         }
     }
 
+    // <editor-fold desc="Unused event methods">
     @Override
     public void keyTyped(KeyEvent e) {
     }
@@ -280,4 +324,5 @@ public class DarkMatter_CenterView extends JComponent implements KeyListener, Mo
     @Override
     public void mouseExited(MouseEvent e) {
     }
+    // </editor-fold>
 }
