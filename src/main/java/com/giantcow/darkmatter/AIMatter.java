b16c8f398cbc8ccbb7af63ffd59cb78c80fca7a5
Charles Horrell <cxh900@cs.bham.ac.uk>
2011-02-24 13:51:12 +0000

    Added a crude Timer to AI player to control movement. .

--- src/main/java/com/giantcow/darkmatter/AIMatter.java
+++ src/main/java/com/giantcow/darkmatter/AIMatter.java
@@ -15,64 +15,72 @@ import java.util.Set;
  */
 public class AIMatter extends Matter {
 
+    int pause = 0;
+
     public AIMatter(double x, double y, double radius, double dy, double dx) {
         super(new Ellipse2D.Double(x, y, radius, radius), dy, dx);
     }
 
     @Override
     public Matter changeMove(double x, double y, Set<Matter> others) {
-        // Calculate expelled matter's new speed
-        Matter best = (Matter)others.toArray()[0];
-        Matter expel = new Matter(5.0, 5.0, 5.0, 0.1, 0.1);
-        for (Matter other : others) {
-            if (Math.hypot(other.getCenterX() - getCenterX(),
-                    other.getCenterY() - getCenterY()) <= Math.hypot(best.getCenterX() - getCenterX(),
-                    best.getCenterY() - getCenterY())) {
-                if (other.getArea() < best.getArea()) {
-                    best = other;
+
+        if (pause++ == 100) {
+            pause = 0;
+            // Calculate expelled matter's new speed
+            Matter best = (Matter) others.toArray()[0];
+            Matter expel = new Matter(5.0, 5.0, 5.0, 0.1, 0.1);
+            for (Matter other : others) {
+                if (Math.hypot(other.getCenterX() - getCenterX(),
+                        other.getCenterY() - getCenterY()) <= Math.hypot(best.getCenterX() - getCenterX(),
+                        best.getCenterY() - getCenterY())) {
+                    if (other.getArea() < best.getArea()) {
+                        best = other;
+                    }
+
                 }
-
             }
+
+            double xmod = Math.abs(getCenterX() - best.getCenterX());
+            double ymod = Math.abs(getCenterY() - best.getCenterY());
+
+            if (getCenterX() < best.getCenterX()) {
+                x = getCenterX() - xmod;
+            }
+
+            if (getCenterX() > best.getCenterX()) {
+                x = getCenterX() + xmod;
+            }
+
+            if (getCenterY() < best.getCenterY()) {
+                y = getCenterY() - ymod;
+            }
+
+
+            if (getCenterY() > best.getCenterY()) {
+                y = getCenterY() + ymod;
+            }
+
+            // Set matter's speed
+            expel.setArea(0.05 * getArea());
+            setArea((1 - 0.05) * getArea());
+            // Calculate matter's position
+            Point2D centre = expulsionCentres(x, y, expel.getRadius());
+            double posX = centre.getX();
+            double posY = centre.getY();
+
+            expel.setFrameFromCenter(posX, posY, posX + expel.getRadius(), posY + expel.getRadius());
+
+            double nextDY = getDy() - expel.getDy();
+            nextDY = (nextDY > MAX_SPEED) ? MAX_SPEED : nextDY;
+            double nextDX = getDx() - expel.getDx();
+            nextDX = (nextDX > MAX_SPEED) ? MAX_SPEED : nextDX;
+            setDy(nextDY);
+            setDx(nextDX);
+
+            return expel;
+        } else {
+            return null;
         }
 
-        double xmod = Math.abs(getCenterX() - best.getCenterX());
-        double ymod = Math.abs(getCenterY() - best.getCenterY());
-
-        if (getCenterX() < best.getCenterX()) {
-            x = getCenterX() - xmod;
-        }
-
-        if (getCenterX() > best.getCenterX()) {
-            x = getCenterX() + xmod;
-        }
-
-        if (getCenterY() < best.getCenterY()) {
-            y = getCenterY() - ymod;
-        }
-
-
-        if (getCenterY() > best.getCenterY()) {
-            y = getCenterY() + ymod;
-        }
-
-        // Set matter's speed
-        expel.setArea(0.05 * getArea());
-        setArea((1 - 0.05) * getArea());
-        // Calculate matter's position
-        Point2D centre = expulsionCentres(x, y, expel.getRadius());
-        double posX = centre.getX();
-        double posY = centre.getY();
-
-        expel.setFrameFromCenter(posX, posY, posX + expel.getRadius(), posY + expel.getRadius());
-
-        double nextDY = getDy() - expel.getDy();
-        nextDY = (nextDY > MAX_SPEED) ? MAX_SPEED : nextDY;
-        double nextDX = getDx() - expel.getDx();
-        nextDX = (nextDX > MAX_SPEED) ? MAX_SPEED : nextDX;
-        setDy(nextDY);
-        setDx(nextDX);
-
-        return expel;
     }
-
 }
