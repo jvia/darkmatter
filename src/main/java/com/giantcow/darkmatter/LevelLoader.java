68dca5f1d7bcdcb013d5e709b3d707f5ee3584e4
Jeremiah Via <jeremiah.via@gmail.com>
2011-03-02 08:37:02 +0000

    Added the stub for a LevelLoader class. Will implement it later today so we can have some basic level editing in our game.

---
+++ src/main/java/com/giantcow/darkmatter/LevelLoader.java
@@ -0,0 +1,22 @@
+package com.giantcow.darkmatter;
+
+/**
+ * Loads files containing a level layout. A level can be laid out in a
+ * text file like this:
+ *
+ * <code>
+ *    x: 0.0 y: 0.0 r: 40.0 dy: 0.1 dx: 0.2
+ *    x: 3.0 y: 3.0 r: 40.0 dy: 0.1 dx: 0.2
+ *    x: 5.0 y: 5.0 r: 40.0 dy: 0.1 dx: 0.2
+ *    x: 7.0 y: 7.0 r: 40.0 dy: 0.1 dx: 0.2
+ * </code>
+ * 
+ * This class will read them in and return them as a set that the game
+ * can use. To make organizing them simple, name your levels like
+ * <code>01.level</code>
+ *
+ * @author Jeremiah M. Via <jxv911@cs.bham.ac.uK>
+ */
+public class LevelLoader {
+    
+}
